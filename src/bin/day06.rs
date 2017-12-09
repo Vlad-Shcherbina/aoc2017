802ed626d4a9dad2442e4819f6b6f4b542ee5eb6
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-12-09 21:49:12 +0300

    Day 6 part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,14 +1,14 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
     // let mut banks = vec![0, 2, 7, 0];
     let mut banks = vec![0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11];
     let n = banks.len();
 
-    let mut seen = HashSet::new();
+    let mut seen = HashMap::new();
     let mut cnt = 0;
-    while !seen.contains(&banks) {
-        seen.insert(banks.clone());
+    while !seen.contains_key(&banks) {
+        seen.insert(banks.clone(), cnt);
         let max_idx = (0..n).min_by_key(|&i| -banks[i]).unwrap();
         let t = banks[max_idx];
         banks[max_idx] = 0;
@@ -18,4 +18,5 @@ fn main() {
         cnt += 1;
     }
     println!("{}", cnt);
+    println!("{}", cnt - seen[&banks]);
 }
