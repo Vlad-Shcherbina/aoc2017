895bbbcdc9a08373d3e7324c41f2fe5bdb6e1e8c
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-12-10 00:03:35 +0300

    Day 9 part 1

---
+++ src/bin/day09.rs
@@ -0,0 +1,61 @@
+use std::iter::Iterator;
+
+#[derive(Debug)]
+enum Node {
+    Garbage,
+    Group(Vec<Node>),
+}
+
+fn parse_garbage<I: Iterator<Item=char>>(it: &mut I) {
+    loop {
+        match it.next().unwrap() {
+            '!' => { it.next().unwrap(); }
+            '>' => break,
+            _ => {}
+        }
+    }
+}
+
+fn parse_node<I: Iterator<Item=char>>(it: &mut I) -> Option<Node> {
+    match it.next().unwrap() {
+        '<' => {
+            parse_garbage(it);
+            Some(Node::Garbage)
+        }
+        '{' => {
+            let mut children = Vec::new();
+            match parse_node(it) {
+                Some(c) => { children.push(c); }
+                None => return Some(Node::Group(children))
+            }
+            loop {
+                match it.next().unwrap() {
+                    ',' => {}
+                    '}' => break,
+                    _ => panic!(),
+                }
+                children.push(parse_node(it).unwrap());
+            }
+            Some(Node::Group(children))
+        }
+        '}' => None,
+        c => panic!("{}", c),
+    }
+}
+
+fn sum_score(node: &Node, depth: i32) -> i32 {
+    match node {
+        &Node::Garbage => 0,
+        &Node::Group(ref ns) => {
+            ns.iter().map(|n| sum_score(n, depth + 1)).sum::<i32>() + depth
+        }
+    }
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut line = String::new();
+    stdin.read_line(&mut line).unwrap();
+    let g = parse_node(&mut line.chars()).unwrap();
+    println!("{}", sum_score(&g, 1));
+}
