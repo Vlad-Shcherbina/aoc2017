28e3a1823a7a7ee0c9998a9460b1b7583db9ff56
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-12-11 01:34:39 +0300

    Day 10 part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,18 +1,31 @@
-// const N: usize = 5;
-// const LENS: &[usize] = &[3, 4, 1, 5];
+#[macro_use] extern crate aoc2017;
+
 const N: usize = 256;
-const LENS: &[usize] = &[120,93,0,90,5,80,129,74,1,165,204,255,254,2,50,113];
+const INPUT: &str = "120,93,0,90,5,80,129,74,1,165,204,255,254,2,50,113";
 
 fn main() {
+    let mut lens = INPUT.to_owned().into_bytes();
+    lens.extend(&[17, 31, 73, 47, 23]);
+    debug!(lens);
     let mut arr: Vec<usize> = (0..N).collect();
     let mut skip = 0;
     let mut start = 0;
-    for &len in LENS {
-        for i in 0..len/2 {
-            &mut arr.swap((start + i) % N, (start + len - 1 - i) % N);
+    for _round in 0..64 {
+        for &len in &lens {
+            let len = len as usize;
+            for i in 0..len/2 {
+                &mut arr.swap((start + i) % N, (start + len - 1 - i) % N);
+            }
+            start += len + skip;
+            skip += 1;
         }
-        start += len + skip;
-        skip += 1;
     }
-    println!("{}", arr[0] * arr[1]);
+    for i in 0..16 {
+        let mut x = 0;
+        for j in 0..16 {
+            x ^= arr[i * 16 + j];
+        }
+        print!("{:02x}", x);
+    }
+    println!();
 }
