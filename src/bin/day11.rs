281497041d092aba501acfddb5caa5a1e22b6d3c
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-12-11 17:30:47 +0300

    Day 11 part 2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -6,6 +6,7 @@ fn main() {
     let mut x = 0i32;
     let mut y = 0;
     let mut z = 0;
+    let mut max_dist = 0;
     for step in line.split(',') {
         match step {
             "n"  => { z += 1; }
@@ -16,8 +17,9 @@ fn main() {
             "nw" => { x -= 1; }
             _ => panic!("{}", step)
         }
+        max_dist = max_dist.max((x - y).abs());
+        max_dist = max_dist.max((y - z).abs());
+        max_dist = max_dist.max((z - x).abs());
     }
-    let ds = [(x - y).abs(), (y - z).abs(), (z - x).abs()];
-    let result = ds.into_iter().max().unwrap();
-    println!("{}", result);
+    println!("{}", max_dist);
 }
