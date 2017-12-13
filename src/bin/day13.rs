48bbb557d12dd58a1b17c9c6d9c7a7f75d10a39a
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-12-13 13:52:52 +0300

    Day 13 part 2

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,17 +1,30 @@
+extern crate time;
+
 use std::io::{BufRead, BufReader};
+
 fn main() {
-    let mut severity = 0;
+    let mut scanners = Vec::<(i32, i32)>::new();
     for line in BufReader::new(std::io::stdin()).lines() {
         let line = line.unwrap();
         let mut it = line.split(": ");
         let depth = it.next().unwrap();
         let range = it.next().unwrap();
         assert!(it.next().is_none());
-        let depth: i32 = depth.parse().unwrap();
-        let range: i32 = range.parse().unwrap();
-        if depth % (2 * range - 2) == 0 {
-            severity += depth * range;
+        scanners.push((depth.parse().unwrap(), range.parse().unwrap()));
+    }
+    let start = time::precise_time_s();
+    for delay in 0.. {
+        let mut caught = false;
+        for &(depth, range) in &scanners {
+            if (depth + delay) % (2 * range - 2) == 0 {
+                caught = true;
+                break;
+            }
+        }
+        if !caught {
+            println!("{}", delay);
+            break;
         }
     }
-    println!("{}", severity);
+    println!("it took {}", time::precise_time_s() - start);
 }
