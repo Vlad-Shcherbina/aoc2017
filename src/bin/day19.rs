92fe32bd5e615fdc7f036165b17d83706ed0bc1a
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-12-19 19:54:54 +0300

    Day 19 part 1

---
+++ src/bin/day19.rs
@@ -0,0 +1,32 @@
+use std::io::BufRead;
+
+fn main() {
+    let stdin = std::io::stdin();
+    let lines: Vec<_> = stdin.lock().lines()
+        .map(|line| line.unwrap().into_bytes()).collect();
+
+    let mut x = lines[0].iter().position(|&c| c == '|' as u8).unwrap() as i32;
+    let mut y = 0;
+    let mut dx = 0;
+    let mut dy = 1;
+
+    let mut s = String::new();
+    loop {
+        match lines[y as usize][x as usize] as char {
+            ' ' => break,
+            '+' => {
+                let t = dx;
+                dx = dy;
+                dy = -t;
+                if lines[(y + dy) as usize][(x + dx) as usize] == ' ' as u8 {
+                    dx = -dx;
+                    dy = -dy;
+                }
+            }
+            '|' | '-' => {}
+            c => s.push(c),
+        }
+        x += dx; y += dy;
+    }
+    println!("{}", s);
+}
