ee323fc589eac8633cd020dbe367334c4f87bddb
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-12-22 16:43:28 +0300

    Day 22 part 2

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -1,8 +1,16 @@
 use std::io::BufRead;
 
+enum State {
+    Clean,
+    Weakened,
+    Infected,
+    Flagged,
+}
+use State::*;
+
 fn main() {
     let stdin = std::io::stdin();
-    let mut infected = std::collections::HashSet::new();
+    let mut grid = std::collections::HashMap::new();
 
     let mut h = 0;
     let mut w = 0;
@@ -13,7 +21,7 @@ fn main() {
         for (j, c) in line.chars().enumerate() {
             match c {
                 '.' => {}
-                '#' => { infected.insert((j as i32, i as i32)); }
+                '#' => { grid.insert((j as i32, i as i32), Infected); }
                 _ => panic!(),
             }
             w += 1;
@@ -25,18 +33,31 @@ fn main() {
     let mut dx = 0;
     let mut dy = -1;
     let mut cnt = 0;
-    for _ in 0..10000 {
-        if infected.insert((x, y)) {
-            let t = dy;
-            dy = -dx;
-            dx = t;
-            cnt += 1;
-        } else {
-            let t = dx;
-            dx = -dy;
-            dy = t;
-            infected.remove(&(x, y));
-        }
+    for _ in 0..10_000_000 {
+        let e = grid.entry((x, y)).or_insert(Clean);
+        *e = match *e {
+            Clean => {
+                let t = dy;
+                dy = -dx;
+                dx = t;
+                Weakened
+            }
+            Weakened => {
+                cnt += 1;
+                Infected
+            }
+            Infected => {
+                let t = dx;
+                dx = -dy;
+                dy = t;
+                Flagged
+            }
+            Flagged => {
+                dx = -dx;
+                dy = -dy;
+                Clean
+            }
+        };
         x += dx;
         y += dy;
     }
