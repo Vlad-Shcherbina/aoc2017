576b8f55c6a9f0aecf4a2a1a62f564fb16409018
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-12-24 14:13:03 +0300

    Day 24 part 1

---
+++ src/bin/day24.rs
@@ -0,0 +1,40 @@
+use std::io::BufRead;
+use std::collections::HashMap;
+
+fn rec(end: i32, adj: &mut HashMap<i32, HashMap<i32, i32>>) -> i32 {
+    let vs: Vec<i32> = adj[&end].iter()
+        .filter_map(|(&v, &cnt)| if cnt > 0 { Some(v) } else { None })
+        .collect();
+    let mut best = 0;
+    for v in vs {
+        *adj.get_mut(&end).unwrap().get_mut(&v).unwrap() -= 1;
+        *adj.get_mut(&v).unwrap().get_mut(&end).unwrap() -= 1;
+        best = best.max(end + v + rec(v, adj));
+        *adj.get_mut(&end).unwrap().get_mut(&v).unwrap() += 1;
+        *adj.get_mut(&v).unwrap().get_mut(&end).unwrap() += 1;
+    }
+    best
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut adj = HashMap::new();
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        let mut it = line.split('/');
+        let left: i32 = it.next().unwrap().parse().unwrap();
+        let right: i32 = it.next().unwrap().parse().unwrap();
+        assert!(it.next().is_none());
+        {
+            let e = adj.entry(left).or_insert_with(HashMap::new);
+            let ee = e.entry(right).or_insert(0);
+            *ee += 1;
+        }
+        {
+            let e = adj.entry(right).or_insert_with(HashMap::new);
+            let ee = e.entry(left).or_insert(0);
+            *ee += 1;
+        }
+    }
+    println!("{}", rec(0, &mut adj));
+}
